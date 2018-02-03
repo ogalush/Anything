893543c78876a6138d9fb121daaf3d0bdb1faab8
ogalush <take_oga@hotmail.com>
2018-02-03 20:12:25 +0900

    add git clone shell

--- gitclone.sh
+++ gitclone.sh
@@ -5,4 +5,5 @@ for i in 'Anything' 'cookbook' 'Pike-InstallConfigs'; do
  rm -rfv $i
  git clone https://github.com/ogalush/${i}.git
 done
+find */.git -name config -exec sed -i 's/github.com/ogalush@github.com/' {} \;
 set +x
