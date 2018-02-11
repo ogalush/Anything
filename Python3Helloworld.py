2f97b237318eb3afd3bfc48ff706a411ccfb9a1d
ogalush <take_oga@hotmail.com>
2018-02-11 21:40:51 +0900

    add Python3Helloworld.py

---
+++ Python3Helloworld.py
@@ -0,0 +1,54 @@
+# -*- coding: UTF-8 -*-
+import sys
+import re
+
+## URL
+# https://docs.python.jp/3/tutorial/
+
+## For Python3
+#print("HelloWorld")
+##print("ほげほげ")
+
+## add
+##num = 10 + 5
+##print(num)
+
+
+### HereDocument
+##print("""\
+##Usage: thingy [OPTIONS]
+##     -h                        Display this usage message
+##     -H hostname               Hostname to connect to
+##""")
+
+## index
+#word = 'Python'
+#print(word[0:3])
+
+## list
+##list = [1, 4, 9, 16, 25]
+##print(list[0:3])
+
+
+## if
+##num = 1
+##if num > 0:
+##  print("Num > 0 (True)")
+##else:
+##  print("Num <= 0 (False)")
+
+## for
+##words = ['cat', 'window', 'defenestrate']
+##for w in words:
+##  print(w,len(w))
+
+## for(Range)
+##for i in range(0, 20, 2):
+## print(i)
+
+## Function
+##def forLoop(n = 10):
+##  for i in range(0, n, 1):
+##    print(i)
+##forLoop(20)
+##forLoop()
