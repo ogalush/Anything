bcaebf2c9040f831f814a9ece2b5ffc09d97d064
ogalush <take_oga@hotmail.com>
2019-02-11 17:03:40 +0900

    CentOS7 cloud-imageの初期化方法追記

--- cent7init.sh
+++ cent7init.sh
@@ -2,4 +2,12 @@
 
 set -x
 ansible-playbook -i '192.168.0.210,' cent7init.yml --user=root --become -K -k
+
+## CloudImage
+## $ ssh -i ~/.ssh/id_rsa_chef centos@192.168.0.103
+## $ sudo su -
+## $ passwd centos
+## → CentOSアカウントのパスワード初期化
+## $ ansible-playbook -i '192.168.0.103,' cent7init.yml --user=centos --become -K --private-key=~/.ssh/id_rsa_chef
 set +x
+
