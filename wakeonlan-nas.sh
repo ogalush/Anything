7a98e7b1e86c65883fc63f69f8bba9018176c80b
ogalush <take_oga@hotmail.com>
2020-10-26 02:22:14 +0900

    Update Comment wakeonlan-nas.sh

--- wakeonlan-nas.sh
+++ wakeonlan-nas.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 #==== Information
+## Check
 # $ sudo arping -c 1 192.168.3.248
 # ARPING 192.168.3.248
 # 60 bytes from 24:5e:be:22:dc:b3 (192.168.3.248): index=0 time=414.623 usec
+## Install
+#  sudo apt -y install wakeonlan
 #==== Information (END)
 set -x
 /usr/bin/wakeonlan 24:5e:be:22:dc:b3
