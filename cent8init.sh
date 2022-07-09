27fc31f0ee72c75fc9a6b653a8f4b45789367e08
ogalush <take_oga@hotmail.com>
2022-07-10 00:15:52 +0900

    CentOS8対応

--- cent8init.sh
+++ cent8init.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 set -x
-ansible-playbook -i '192.168.3.200,' cent8init.yml --user=root --become -bkK
+ansible-playbook -i '192.168.3.210,' cent8init.yml --user=centos -bkK
 
 ## CloudImage
 ## $ ssh -i ~/.ssh/id_rsa_chef centos@192.168.0.103
 ## $ sudo su -
 ## $ passwd centos
 ## → CentOSアカウントのパスワード初期化
-## $ ansible-playbook -i '192.168.3.200,' cent8init.yml --user=centos --become -K --list-hosts
-## $ ansible-playbook -i '192.168.3.200,' cent8init.yml --user=centos --become -K
+## $ ansible-playbook -i '192.168.3.200,' cent8init.yml --user=centos --become -kK --list-hosts
+## $ ansible-playbook -i '192.168.3.200,' cent8init.yml --user=centos --become -kK
 
 ## Only OS Update
 ## ansible-playbook -i '192.168.3.200,' cent8init.yml --become -bK --skip-tags='*' --tags='dnf_update' --list-tags
